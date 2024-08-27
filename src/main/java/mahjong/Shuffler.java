6b7b862839890d6ac4afd88ed5d44ea6fb5bee2e
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-08-27 19:45:58 -0300

    format Shuffler.java

--- src/main/java/mahjong/Shuffler.java
+++ src/main/java/mahjong/Shuffler.java
@@ -4,37 +4,33 @@ import java.lang.*;
 import java.util.*;
 
 public class Shuffler {
-	
-	Shuffler(){
-		setSize(136);	
+	Shuffler() {
+		setSize(136);
 	}
 
-	Shuffler(int N){
-		setSize(N);	
+	Shuffler(int N) {
+		setSize(N);
 	}
-	
+
 	public int[] index;
 	public int count = 0;
 	private int left = 14;
 
-
-	
-	public void setSize(int N){					
-		if (index == null || N != index.length){
+	public void setSize(int N) {
+		if (index == null || N != index.length) {
 			index = new int[N];
 			initializeIndex();
 			permuteIndex();
 		}
 	}
-	
-	public void initializeIndex(){				
-		for(int i=0;i<index.length;i++)
-			index[i] = i;
+
+	public void initializeIndex() {
+		for (int i=0;i<index.length;i++) index[i] = i;
 	}
 
-	public void permuteIndex(){					
+	public void permuteIndex(){
 		java.util.Random rnd = new java.util.Random();
-		for(int i=index.length-1;i>=0;i--){
+		for (int i=index.length-1;i>=0;i--) {
 			int j = rnd.nextInt(i+1);
 			int tmp = index[j];
 			index[j] = index[i];
@@ -44,21 +40,20 @@ public class Shuffler {
 		count = 0;
 	}
 
-	public void ackKong(){
+	public void ackKong() {
 		left++;
 	}
 
-	public Tile getNext(){		
-		Tile res;				
+	public Tile getNext() {
+		Tile res;
 		int next = index[count++];
-		if (count > index.length - left){
+		if (count > index.length - left) {
 			permuteIndex();
 			return null;
 		}
-	
+
 		res = new Tile(next/4);
-		
+
 		return res;
 	}
 }
-
