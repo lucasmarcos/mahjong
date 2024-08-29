23b498b73a065f1aa510fe7e3141959f95ebc907
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-08-29 17:19:37 -0300

    test example

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+class FirstTest {
+    @Test
+    void first() {
+        assertEquals(1, 1);
+    }
+}
