714fe199594a0d859c7f44b0033bed7a9c0fe3a9
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-08-29 22:42:31 -0300

    Test Hand toString

--- src/test/java/HandTest.java
+++ src/test/java/HandTest.java
@@ -23,4 +23,20 @@ class HandTest {
 
         assertEquals(all, list);
     }
+
+    @Test
+    void string() {
+        var list = new ArrayList<ArrayList<Tile>>();
+
+        list.add(new ArrayList<Tile>());
+        list.add(new ArrayList<Tile>());
+        list.add(new ArrayList<Tile>());
+        list.add(new ArrayList<Tile>());
+
+        var hand = new Hand(list);
+
+        var string = hand.toString();
+
+        assertEquals("", string);
+    }
 }
