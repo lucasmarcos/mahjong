aafccd407a00a77429208cac3835155a42f301b9
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-08-30 19:05:03 -0300

    Board.java: alterações do William

--- src/main/java/mahjong/Board.java
+++ src/main/java/mahjong/Board.java
@@ -4,198 +4,259 @@ import java.util.ArrayList;
 
 public class Board {
     public static final int initScore = 25000;
-    public static final int games = 1;    //1東風戰 2東南戰... {1 Guerra Dongfeng, 2 Guerra Sudeste...}
-    public static int wind;    //0東 1南 2西 3北 {0 Leste 1 Sul 2 Oeste 3 Norte}
-    public static int game;    //現在的局數-1 {Contagem de jogos atual -1}
+    public static final int games = 1;
+    public static int wind = 0;
+    public static int game = 0;
     public static String[] actionString = {
         "",
-        "comer", // comer 吃
-        "ressalto", // ressalto 碰
-        "barra", // barra 槓
-        "Adicionar uma barra", // Adicionar uma barra 加槓
-        "barra escondida", // barra escondida 暗槓
-        "fique em linha reta", // fique em linha reta 立直
-        "glória", // glória 榮
-        "Hu" // Hu 胡
+        "comer",
+        "ressalto",
+        "barra",
+        "Adicionar uma barra",
+        "barra escondida",
+        "fique em linha reta",
+        "glória",
+        "Hu",
     };
-    public static int dealer; //一開始的莊家 {O banqueiro no início}
+    public static int dealer = 0;
+    private static Shuffler shuffler;
+    private static ComGUI GUI;
+    private static Player[] players = new Player[4];
+    private static ArrayList<ArrayList<Tile>> allTiles = new ArrayList<>();
+    private static ArrayList<ArrayList<Tile>> table = new ArrayList<>();
+    private static int[] left = { 0, 0, 0, 0 };
+
+    ///
+    public static void main(String[] args) {
+        initGame(); // Inicializa o jogo (configurações, variáveis, etc.)
+
+        while (true) { // Loop principal do jogo
+            startNewRound(); // Inicia uma nova rodada do jogo
+
+            if (playRound() || endGameConditionMet()) break;
+            // Chama o método 'playRound' para jogar a rodada. Se 'playRound' retornar 'true' (ou seja, o jogo terminou)
+            // ou se a condição de fim de jogo ('endGameConditionMet') for satisfeita, o loop termina e o jogo acaba.
+        }
+    }
+
+    private static boolean playRound() {
+        int gameOver = 0; // Variável para checar se o jogo acabou
+        int current = (dealer + game) % 4; // Determina qual jogador é o atual baseado no dealer e no estado do jogo
+        Tile tile = shuffler.getNext(); // Pega a próxima peça do embaralhador (shuffler)
+        Action action = players[current].doSomething(0, tile); // O jogador faz uma ação baseada na peça que recebeu
+
+        while (gameOver == 0) { // Loop até que a variável 'gameOver' seja alterada
+            debugRound(current, action); // Chama o método 'debugRound' para imprimir as informações de depuração
+            gameOver = handlePlayerAction(current, action); // Processa a ação do jogador atual e verifica se o jogo acabou
+
+            if (gameOver == 1) return true; // Se o jogo acabou (gameOver == 1), a rodada termina e o método retorna 'true'
+
+            tile = shuffler.getNext(); // Pega a próxima peça
+            if (tile == null) return handleDrawSituation() == 1; // Se não houver mais peças (tile == null), trata a situação de empate e retorna 'true' se o jogo acabou
+
+            action = players[current].doSomething(0, tile); // O jogador faz outra ação com a nova peça
+        }
+        return false; // Se o loop terminou sem game over, a rodada não resultou no fim do jogo
+    }
+
+    private static void debugRound(int current, Action action) {
+        System.out.println(
+            "DEBUG: wind: " +
+            wind +
+            " game: " +
+            game +
+            players[current] +
+            actionString[action.type] +
+            "."
+        );
+    }
+
+    ///
+
+    private static void initGame() {
+        shuffler = new Shuffler();
+        GUI = new ComGUI();
+        GUI.initPlayerGUI("PlayerGUI", initScore, GUI);
+        players[0] = GUI.player;
+
+        for (int i = 1; i < 4; i++) {
+            players[i] = new AI("PlayerAI" + i, initScore);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            allTiles.add(new ArrayList<>());
+            table.add(new ArrayList<>());
+        }
+        table.add(new ArrayList<>()); // Adiciona o fundo do rio
+    }
+
+    private static void startNewRound() {
+        table.get(0).clear(); // Limpa o fundo do rio
+
+        for (int i = 0; i < 4; i++) {
+            table.get(i + 1).clear(); // Limpa os efeitos colaterais
+            left[i] = 13; // 13 cartas na mão
+            if (i > 0) GUI.assignHandNum(i + 1, left[i]);
+
+            allTiles.forEach(ArrayList::clear); // Limpa todas as tiles
+            for (int j = 0; j < 13; j++) {
+                Tile tmpTile = shuffler.getNext();
+                allTiles.get(tmpTile.suit).add(tmpTile);
+            }
+
+            players[i].initHand(allTiles);
+        }
+
+        GUI.showWind(wind, game + 1);
+        GUI.renewGUI();
+        GUI.showGUI();
+    }
+
+    private static int handlePlayerAction(int current, Action action) {
+        switch (action.type) {
+            case 0:
+            case 1:
+            case 2:
+            case 6: // Ações: tocar, comer, ressalto, linha reta
+                return handleStandardAction(current, action);
+            case 3:
+            case 4:
+            case 5: // Ações: barra, adicionar barra, barra escondida
+                return handleKongAction(current, action);
+            case 7:
+            case 8: // Ações: glória, toque-se
+                return handleGameOverAction(current, action);
+            default:
+                System.out.println(
+                    "ERROR: " +
+                    players[current] +
+                    " unknown action " +
+                    action.type +
+                    "."
+                );
+                System.exit(1);
+                return 0;
+        }
+    }
+
+    private static int handleStandardAction(int current, Action action) {
+        if (current > 0) {
+            left[current] -= (action.tiles.size() - 1);
+            GUI.assignHandNum(current + 1, left[current]);
+        }
+
+        for (int i = 1; i < action.tiles.size(); i++) {
+            table.get(current + 1).add(action.tiles.get(i));
+        }
+
+        GUI.assignTile(table);
+        GUI.renewGUI();
+        Tile tile = action.tiles.get(0);
+        int selectPlayer = findNextActionPlayer(current, tile);
+
+        if (selectPlayer != -1) {
+            return 0; // Continues to next action
+        } else {
+            table.get(0).add(tile);
+            GUI.assignTile(table);
+            GUI.renewGUI();
+            return 0;
+        }
+    }
+
+    private static int handleKongAction(int current, Action action) {
+        if (current > 0) {
+            left[current] -= action.tiles.size();
+            GUI.assignHandNum(current + 1, left[current]);
+        }
+
+        for (Tile tile : action.tiles) {
+            table.get(current + 1).add(tile);
+        }
+
+        GUI.assignTile(table);
+        GUI.renewGUI();
+        shuffler.ackKong();
+        return 0;
+    }
+
+    private static int handleGameOverAction(int current, Action action) {
+        printTiles(action.tiles);
+
+        if (current != (dealer + game) % 4) {
+            game++;
+        }
+
+        shuffler.permuteIndex();
+        if (current > 0) {
+            GUI.flipTile(current - 1, action.tiles);
+        }
+
+        for (int i = 0; i < 4; i++) {
+            players[i].GameOver(
+                    action.type == 7 ? 1 : 2,
+                    (current - i + 4) % 4
+                );
+        }
+
+        return 1;
+    }
+
+    private static int handleDrawSituation() {
+        for (int i = 0; i < 4; i++) {
+            players[i].GameOver(0, i); // Notifica sobre a situação de empate
+        }
+        return 1;
+    }
+
+    private static boolean endGameConditionMet() {
+        if (game == 4) {
+            wind++;
+            game = 0;
+        }
+
+        if (wind == games) {
+            if (!GUI.showWind(wind, -1)) {
+                return true;
+            }
+            game = 0;
+            wind = 0;
+        }
+
+        return false;
+    }
+
+    private static int findNextActionPlayer(int current, Tile tile) {
+        Action selectAction = null;
+        int selectPlayer = -1;
+
+        for (int i = 1; i < 4; i++) {
+            int p = (current + i) % 4;
+            System.out.println("wait " + p + " " + tile + " " + tile.getSize());
+            Action action = players[p].doSomething(4 - i, tile);
+
+            if (action == null) continue;
+
+            System.out.println(p + " " + actionString[action.type]);
+            if (selectPlayer == -1 || action.type > selectAction.type) {
+                if (selectPlayer != -1) players[selectPlayer].failed();
+                selectAction = action;
+                selectPlayer = p;
+            } else {
+                players[p].failed();
+            }
+        }
+
+        if (selectAction != null) {
+            current = selectPlayer;
+        }
+
+        return selectPlayer;
+    }
 
     public static void printTiles(ArrayList<Tile> tiles) {
         for (Tile t : tiles) {
             System.out.print(t.toString() + t.getSize() + ",");
-        }
-    }
-
-    public static void main(String[] args) {
-        wind = 0;
-        dealer = 0;    //maybe we should decide this randomly? {talvez devêssemos decidir isso aleatoriamente?}
-        game = 0;
-
-        Shuffler shuffler = new Shuffler();
-        ComGUI GUI = new ComGUI();
-        Player[] player = new Player[4];
-
-        ArrayList<ArrayList<Tile>> allTiles = new ArrayList<ArrayList<Tile>>();// 0萬 1筒 2條 3字 {0 = Dez mil, 1 = cilindro, 2 = faixa, 3 = Personagem}
-        ArrayList<ArrayList<Tile>> table = new ArrayList<ArrayList<Tile>>();
-
-        int[] left = {0, 0, 0, 0};
-
-        GUI.initPlayerGUI("PlayerGUI", initScore, GUI);
-        player[0] = GUI.player;
-        table.add(new ArrayList<Tile>());    //河底 {fundo do rio}
-
-        for (int i = 1; i < 4; i++) {
-            player[i] = new AI("PlayerAI" + i, initScore);
-        }
-
-        for (int i = 0; i < 4; i++) {
-            allTiles.add(new ArrayList<Tile>());
-            table.add(new ArrayList<Tile>());    //副露 {Vice-exposição}
-        }
-
-        while (true) {
-            table.getFirst().clear();    //清空河底 {Limpe o fundo do rio}
-
-            //init 4 players' hands and tables
-            for (int i = 0; i < 4; i++) {
-                table.get(i + 1).clear();//清空副露 {efeitos colaterais claros}
-                left[i] = 13;//手牌13張 {13 cartas na mão}
-                if (i > 0) GUI.assignHandNum(i + 1, left[i]);
-                for (int j = 0; j < 4; j++) {
-                    allTiles.get(j).clear();
-                }
-                for (int j = 0; j < 13; j++) {
-                    Tile tmpTile = shuffler.getNext();
-                    allTiles.get(tmpTile.suit).add(tmpTile);
-                }
-                player[i].initHand(allTiles);
-            }
-
-            GUI.showWind(wind, game + 1);
-            GUI.renewGUI();
-            GUI.showGUI();
-
-            int current = (dealer + game) % 4;//看第幾局決定輪到誰做莊，莊家開始，抽牌、決定動作 {Determine de quem é a vez de ser o dealer, dependendo da rodada em que o dealer está. O dealer começa, compra cartas e decide as ações.}
-            Tile tile = shuffler.getNext();
-            Action action = player[current].doSomething(0, tile);
-
-            boolean gameOver = false;
-            while (!gameOver) {
-                System.out.println("DEBUG: " + "wind: " + wind + "game: " + game + player[current] + actionString[action.type] + ".");
-
-                switch (action.type) {//執行動作 {executar a ação}
-                    case 0:    //摸 {tocar}
-                    case 1:    //吃 {comer}
-                    case 2:    //碰 {ressalto}
-                    case 6:    //立直 {fique em linha reta}
-                        if (current > 0) {//手牌減少 {Mãos reduzidas}
-                            left[current] -= (action.tiles.size() - 1);
-                            GUI.assignHandNum(current + 1, left[current]);
-                        }
-
-                        for (int i = 1; i < action.tiles.size(); i++) {    //副露 {Vice-exposição}
-                            table.get(current + 1).add(action.tiles.get(i));
-                        }
-
-                        GUI.assignTile(table);
-                        GUI.renewGUI();
-
-                        tile = action.tiles.getFirst();    //打出來的牌 {cartas jogadas}
-                        Action selectAction = null;
-                        int selectPlayer = -1;
-                        for (int i = 1; i < 4; i++) {//問另外三家有沒有事情要做 {Pergunte às outras três empresas se elas têm algo a fazer.}
-                            int p = (current + i) % 4;
-                            System.out.println("wait " + p + " " + tile + " " + tile.getSize());
-                            action = player[p].doSomething(4 - i, tile);
-                            if (action == null) continue;
-                            System.out.println(p + " " + actionString[action.type]);
-                            if (selectPlayer == -1 || action.type > selectAction.type) {
-                                if (selectPlayer != -1)
-                                    player[selectPlayer].failed();
-                                selectAction = action;
-                                selectPlayer = p;
-                            } else player[p].failed();
-                        }
-
-                        if (selectAction != null) {//執行最優先動作, 榮>碰>吃, 設定好動作、玩家後continue跳到該玩家執行動作，未考慮同時榮的情形:p {Execute a ação de maior prioridade, glória> toque> comer, após definir a ação e o jogador, continue saltando para o jogador para realizar a ação, sem considerar a situação de glória simultânea:p}
-                            action = selectAction;
-                            current = selectPlayer;
-                            continue;
-                        } else {//換下一家，到switch外面抽牌、決定動作 {Mude para a próxima casa, saia do switch para comprar cartas e decidir a ação}
-                            table.getFirst().add(tile);
-                            GUI.assignTile(table);
-                            GUI.renewGUI();
-                            current = (current + 1) % 4;
-                        }
-                        break;
-
-                    case 3:    //槓 {bar}
-                    case 4:    //加槓 {Adicionar uma barra}
-                    case 5:    //暗槓 {barra escondida}
-                        if (current > 0) {//手牌減少 {Mãos reduzidas}
-                            left[current] -= (action.tiles.size());
-                            GUI.assignHandNum(current + 1, left[current]);
-                        }
-                        for (int i = 0; i < action.tiles.size(); i++) {    //槓從0開始算副露 {Kong começa a contar a partir de 0}
-                            table.get(current + 1).add(action.tiles.get(i));
-                        }
-                        GUI.assignTile(table);
-                        GUI.renewGUI();
-                        shuffler.ackKong();
-                        break;    //目前玩家補一張，到switch外面抽牌、決定動作 {O jogador atualmente compra uma carta e sai do switch para comprar cartas e decidir ações.}
-
-                    case 7:    //榮 {glória}
-                    case 8:    //自摸 {Toque-se}
-                        printTiles(action.tiles);
-                        if (current != (dealer + game) % 4) {//當局莊家沒有連莊就要輪莊，進入下一局 {Se o banqueiro não tiver uma sucessão de banqueiros, ele recorrerá ao banqueiro e entrará na próxima rodada.}
-                            game++;
-                        }
-                        shuffler.permuteIndex();
-                        gameOver = true;
-                        if (current > 0)
-                            GUI.flipTile(current - 1, action.tiles);
-                        for (int i = 0; i < 4; i++) {
-                            if (action.type == 7)
-                                player[i].GameOver(1, (current - i + 4) % 4);    //告知player, current榮 {Diga ao jogador, atual}
-                            else
-                                player[i].GameOver(2, (current - i + 4) % 4);    //告知player, current自摸 {Diga ao jogador, a corrente toca nele}
-                        }
-                        break;
-
-                    default:
-                        System.out.println("ERROR: " + player[current] + " unknown action " + action.type + ".");
-                        System.exit(1);
-                }
-
-                if (gameOver) break;
-
-                tile = shuffler.getNext();//switch外面指的是這裡^^ {A parte externa do switch refere-se aqui ^^}
-
-                if (tile == null) {//流局 {Situação perdida}
-                    gameOver = true;
-                    for (int i = 0; i < 4; i++) {
-                        player[i].GameOver(0, i);    //告知player流局 {Notifique o jogador sobre a situação}
-                    }
-                    break;
-                }
-
-                System.out.println("self " + current + " " + tile + " " + tile.getSize());
-                action = player[current].doSomething(0, tile);
-            }
-
-            if (game == 4) {    //打滿4局，南(?入 {Depois de jogar 4 partidas, Nan (?}
-                wind = wind + 1;
-                game = 0;
-            }
-
-            if (wind == games) {    //結束 {Terminar}
-                if (GUI.showWind(wind, -1)) {
-                    game = 0;
-                    wind = 0;
-                } else {
-                    break;
-                }
-            }
         }
     }
 }
