102ab6d30537098cb3f76bd0a495e3b2f6713bf3
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-09-05 14:50:49 -0300

    .

--- src/test/java/ActionTest.java
+++ src/test/java/ActionTest.java
@@ -1,10 +1,20 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+
 import mahjong.Action;
 
 class ActionTest {
     @Test
-    void action(){
+    void actionsType() {
+        var actions = new Action(0, new ArrayList<>());
+        assertEquals(0, actions.type);
+    }
+
+    @Test
+    void actionsTiles() {
+        var actions = new Action(0, new ArrayList<>());
+        assertEquals(new ArrayList<>(), actions.tiles);
     }
 }
