896a10cbfee4e740b5da33ad786a5de48290ac45
Lucas da Silva Marcos <lucasmarcos@noreply.github.com>
2024-09-05 12:58:06 -0300

    Tile Test compare

---
+++ src/test/java/TileTest.java
@@ -0,0 +1,48 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import mahjong.Tile;
+
+class TileTest {
+    @Test
+    void string0() {
+        var tile0 = new Tile(0);
+        assertEquals("一萬", tile0.toString());
+    }
+
+    @Test
+    void string1() {
+        var tile1 = new Tile(1);
+        assertEquals("二萬", tile1.toString());
+    }
+
+    @Test
+    void compareEq() {
+        var tile1 = new Tile(0);
+        var tile2 = new Tile(0);
+
+        var res = tile1.compareTo(tile2);
+
+        assertEquals(0, res);
+    }
+
+    @Test
+    void compareDiff1() {
+        var tile1 = new Tile(0);
+        var tile2 = new Tile(1);
+
+        var res = tile1.compareTo(tile2);
+
+        assertEquals(-1, res);
+    }
+
+    @Test
+    void compareDiff2() {
+        var tile1 = new Tile(1);
+        var tile2 = new Tile(0);
+
+        var res = tile1.compareTo(tile2);
+
+        assertEquals(1, res);
+    }
+}
